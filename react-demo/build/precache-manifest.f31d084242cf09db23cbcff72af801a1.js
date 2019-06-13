981454a237eaa39dbfec5d5a29101e39ffeff474
John <jazcbook@163.com>
2019-06-13 21:11:51 +0800

    react build

---
+++ react-demo/build/precache-manifest.f31d084242cf09db23cbcff72af801a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdd291d4a3797fe5c35d6f2a7144ace",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e6480cc857e44b3cc3b",
+    "url": "./static/css/main.36dcb603.chunk.css"
+  },
+  {
+    "revision": "d9562a260558f2dd98e7",
+    "url": "./static/js/2.4b4f80f2.chunk.js"
+  },
+  {
+    "revision": "0e6480cc857e44b3cc3b",
+    "url": "./static/js/main.ea8a00d0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
